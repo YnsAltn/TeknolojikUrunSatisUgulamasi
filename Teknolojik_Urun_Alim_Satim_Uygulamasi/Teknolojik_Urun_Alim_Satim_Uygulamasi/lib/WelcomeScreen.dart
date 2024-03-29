6f6e4a8c60ea36934c5f105c72b5db3133951a0a
YnsAltn <ynsaltinn@gmail.com>
2024-03-29 09:16:35 +0300

    Update
    Uygulamanın Giriş Ekran Tasarımı Yapıldı

---
+++ Teknolojik_Urun_Alim_Satim_Uygulamasi/Teknolojik_Urun_Alim_Satim_Uygulamasi/lib/WelcomeScreen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:untitled3/regScreen.dart';
+
+import 'loginScreen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color(0xffB81736),
+          Color(0xff281537),
+        ])),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 200.0),
+              child: Image(image: AssetImage('assets/icons/icon2.png')),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              'Hoşgeldiniz',
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const loginScreen()));
+              },
+              child: Container(
+                height: 53,
+                width: 320,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.white),
+                ),
+                child: const Center(
+                  child: Text(
+                    'GİRİŞ YAP',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const RegScreen()));
+              },
+              child: Container(
+                height: 53,
+                width: 320,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.white),
+                ),
+                child: const Center(
+                  child: Text(
+                    'KAYIT OL',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Teknolojik_Urun_Alim_Satim_Uygulamasi/Teknolojik_Urun_Alim_Satim_Uygulamasi/lib/regScreen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class RegScreen extends StatelessWidget {
+  const RegScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      //thanks for watching
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color(0xffB81736),
+              Color(0xff281537),
+            ]),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.only(top: 60.0, left: 22),
+            child: Text(
+              'HESABINI \n         OLUŞTUR \n                                ...',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 200.0),
+          child: Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+              color: Colors.white,
+            ),
+            height: double.infinity,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18.0, right: 18),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.check,
+                          color: Colors.grey,
+                        ),
+                        label: Text(
+                          'İsim',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        )),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.check,
+                          color: Colors.grey,
+                        ),
+                        label: Text(
+                          'Telefon',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        )),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                        label: Text(
+                          'Şifre',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        )),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                        label: Text(
+                          'Şifre Tekrar',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  Container(
+                    height: 55,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: const LinearGradient(colors: [
+                        Color(0xffB81736),
+                        Color(0xff281537),
+                      ]),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'HESAP OLUŞTUR',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  const Align(
+                    alignment: Alignment.bottomRight,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
